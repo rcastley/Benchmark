06c199328e34b31a3edda60a18951dfc2885cc92
rcastley <robert.castley@gmail.com>
2015-11-27 10:18:03 +0000

    DB Schema init

--- scripts/data.sqlite.sql
+++ scripts/data.sqlite.sql
@@ -2,5 +2,4 @@
 --
 -- You can begin populating the database with the following SQL statements.
 
-INSERT INTO scc (user, state) VALUES
-    ('admin', 'default');
+INSERT INTO users (user) VALUES ('benchmark');
--- scripts/schema.sqlite.sql
+++ scripts/schema.sqlite.sql
@@ -1,12 +1,33 @@
--- scripts/schema.sqlite.sql
---
--- You will need load your database schema with this SQL.
+DROP TABLE users;
+CREATE TABLE users (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  user VARCHAR(255) NOT NULL UNIQUE,
+  key VARCHAR(255),
+  secret VARCHAR(255),
+  cid INTEGER
+);
+CREATE INDEX "id" ON "users" ("id");
 
-CREATE TABLE scc (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    user TEXT NOT NULL UNIQUE,
-    state TEXT NOT NULL,
-    key TEXT,
-    secret TEXT);
+DROP TABLE benchmark_data;
+CREATE TABLE benchmark_data (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  cid INTEGER NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  dns INTEGER NOT NULL,
+  wait INTEGER NOT NULL,
+  load INTEGER NOT NULL,
+  bytes INTEGER NOT NULL,
+  doc_complete INTEGER NOT NULL,
+  webpage_response INTEGER NOT NULL,
+  items INTEGER NOT NULL
+);
+CREATE INDEX bid ON "benchmark_data" ("id");
 
-CREATE INDEX "id" ON "scc" ("id");
+DROP TABLE benchmark_charts;
+CREATE TABLE benchmark_charts (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  cid INTEGER NOT NULL UNIQUE,
+  name VARCHAR(255) NOT NULL,
+  modified TEXT
+);
+CREATE INDEX cid ON "benchmark_charts" ("id");
